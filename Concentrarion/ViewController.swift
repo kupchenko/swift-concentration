711d0c8638abee9e3cca21397a9cc56d5a24bb0e
Dmitrii Kupchenko <dkupchenko@atlassian.com>
2018-10-06 16:21:28 +0300

    Initial Commit

---
+++ Concentrarion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentrarion
+//
+//  Created by Dmitrii Kupchenko on 06/10/2018.
+//  Copyright © 2018 Dmitrii Kupchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
